b88f0c2affbe2e863a766d5ef3b5a5cfdf3f4942
RedFox <azizayari879@gmail.com>
2026-07-09 16:41:42 +0100

    testing beacon

--- src/main.c
+++ src/main.c
@@ -9,17 +9,84 @@
 #include <logger.h>
 #include <zephyr/kernel.h>
 #include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/types.h>
+#include <stddef.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/drivers/gpio.h>
 LOG_MODULE_REGISTER(main_log, LOG_LEVEL_INF);
+
+
+#define DEVICE_NAME CONFIG_BT_DEVICE_NAME
+#define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
+static const struct bt_data ad[] = {
+	BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
+	BT_DATA_BYTES(BT_DATA_UUID16_ALL, 0xaa, 0xfe),
+	BT_DATA_BYTES(BT_DATA_SVC_DATA16,
+		      0xaa, 0xfe, 
+		      0x10, 
+		      0x00, 
+		      0x00, 
+		      'z', 'e', 'p', 'h', 'y', 'r',
+		      'p', 'r', 'o', 'j', 'e', 'c', 't',
+		      0x08) 
+};
+
+
+static const struct bt_data sd[] = {
+	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+};
+
+static void bt_ready(int err)
+{
+	bt_addr_le_t addr = {0};
+	size_t count = 1;
+
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+		return;
+	}
+
+	printk("Bluetooth initialized\n");
+
+	err = bt_le_adv_start(BT_LE_ADV_NCONN_IDENTITY, ad, ARRAY_SIZE(ad),
+			      sd, ARRAY_SIZE(sd));
+	if (err) {
+		printk("Advertising failed to start (err %d)\n", err);
+		return;
+	}
+
+
+
+	bt_id_get(&addr, &count);
+
+	printk("Beacon started, advertising as %s\n", bt_addr_le_str(&addr));
+}
+
 int main(void)
 {
 	init_logging();
+	
 	/*
+	static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+	gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+
+	while (1) {
+		gpio_pin_toggle_dt(&led);
+		k_msleep(500);
+	}
+		*/
 	int err;
+	printf("running bluetooth");
 	err = bt_enable(NULL);
     if (err) {
         printf("Bluetooth init failed (err %d)", err);
         return err;
     }
-	*/
+
+
+
+	
 	return 0;
 }
